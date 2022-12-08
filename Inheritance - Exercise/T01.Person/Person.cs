9becd1e09223ac37d0a091f88d8db1d7c2954378
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-08 19:54:05 +0200

    Completed task: T01.Person

---
+++ Inheritance - Exercise/T01.Person/Person.cs
@@ -0,0 +1,47 @@
+﻿namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    this.age = value;
+                }
+            }
+        }
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.Age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {this.Name}, Age: {this.Age}";
+        }
+    }
+}
+
