aa06cdae44a3684f5314ea152dcc65486da86f61
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-08 19:53:24 +0200

    Completed trask: T04.RandomList

---
+++ Inheritance - Lab/T04.RandomList/RandomList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random rnd;
+
+        public RandomList(Random rnd)
+        {
+            this.rnd = rnd;
+        }
+
+        public string RandomString()
+        {
+            
+            int index = rnd.Next(0, this.Count);
+            string str = this[index];
+            base.RemoveAt(index);
+            return str;
+        }
+    }
+}
