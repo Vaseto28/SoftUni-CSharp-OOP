a042b71a73e1016b4323e70a2da7aac45904a863
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-08 19:53:38 +0200

    Completed task: T05.StackOfStrings

---
+++ Inheritance - Lab/T05.StackOfStrings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CustomStack
+{
+    class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Inheritance - Lab/T05.StackOfStrings/StackOfStrigns.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StackOfStrigns :  Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return this.Count == 0;
+        }
+
+        public void AddRange(IEnumerable<string> collection)
+        {
+            foreach (var item in collection)
+            {
+                this.Push(item);
+            }
+        }
+    }
+}
+
