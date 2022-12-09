22254edb3f61920eed9ff254552f88916630af41
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-09 09:14:40 +0200

    Completed task: T03.ShoppingSpree

---
+++ Encapsulation - Exercise/T03.ShoppingSpree/Person.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T03.ShoppingSpree
+{
+    public class Person
+    {
+        private string name;
+        private decimal money;
+        private readonly List<Product> bagOfProducts;
+
+        public Person()
+        {
+            this.BagOfProducts = new List<Product>();
+        }
+
+        public Person(string name, decimal money) : this()
+        {
+            this.Name = name;
+            this.Money = money;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public decimal Money
+        {
+            get
+            {
+                return this.money;
+            }
+            set
+            {
+                if (value<0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+
+                this.money = value;
+            }
+        }
+
+        public ICollection<Product> BagOfProducts { get; set; }
+    }
+}
+
