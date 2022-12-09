fe28cf1f59d3e6fb54dcd86c342835504bcf62ef
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-09 09:11:23 +0200

    Completed task: T06.Animals

---
+++ Inheritance - Exercise/T06.Animals/StartUp.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            //List<Animal> animals = new List<Animal>();
+            //Dictionary<string, List<Animal>> animals = new Dictionary<string, List<Animal>>();
+
+            List<Animal> animals = new List<Animal>();
+
+            string firstCommand = Console.ReadLine();
+            while (true)
+            {
+                if (firstCommand == "Beast!")
+                {
+                    break;
+                }
+
+                string animalType = firstCommand;
+
+                string secondCommand = Console.ReadLine();
+                string[] secondCommandArgs = secondCommand.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string name = secondCommandArgs[0];
+                string stringAge = secondCommandArgs[1];
+                string gender = secondCommandArgs[2];
+
+                if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(stringAge) || string.IsNullOrEmpty(gender))
+                {
+                    Console.WriteLine("Invalid input!");
+                    firstCommand = Console.ReadLine();
+                    continue;
+                }
+
+                int age = int.Parse(stringAge);
+                if (age < 0)
+                {
+                    Console.WriteLine("Invalid input!");
+                    firstCommand = Console.ReadLine();
+                    continue;
+                }
+
+                if (animalType == "Dog")
+                {
+                    Dog dog = new Dog(name, age, gender);
+                    animals.Add(dog);
+                }
+                else if (animalType == "Cat")
+                {
+                    Cat cat = new Cat(name, age, gender);
+                    animals.Add(cat);
+                }
+                else if (animalType == "Tomcat")
+                {
+                    Tomcat tomcat = new Tomcat(name, age);
+                    animals.Add(tomcat);
+                }
+                else if (animalType == "Kitten")
+                {
+                    Kitten kitten = new Kitten(name, age);
+                    animals.Add(kitten);
+                }
+                else
+                {
+                    Frog frog = new Frog(name, age, gender);
+                    animals.Add(frog);
+                }
+
+                firstCommand = Console.ReadLine();
+            }
+
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(animal);
+            }
+        }
+    }
+}
---
+++ Inheritance - Exercise/T06.Animals/Tomcat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Animals
+{
+    public class Tomcat : Cat
+    {
+        public const string Gender = "Male";
+
+        public Tomcat(string name, int age) : base(name, age, Gender)
+        {
+
+        }
+
+        public override string ProduceSound()
+        {
+            return "MEOW";
+        }
+    }
+}
+
