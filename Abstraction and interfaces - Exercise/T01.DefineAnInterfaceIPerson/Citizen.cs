d489d1329936f196d5d0fce5d94acc79e5a6a5d5
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-11 15:31:12 +0200

    Completed task: T01.DefineAnInterfaceIPerson

---
+++ Abstraction and interfaces - Exercise/T01.DefineAnInterfaceIPerson/Citizen.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using PersonInfo;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public Citizen(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+    }
+}
+
