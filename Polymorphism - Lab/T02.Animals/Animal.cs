3e8b15ea20b1535d3ee3a4ad9f573dc0de4d2b5b
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-14 22:23:30 +0200

    Completed task: T02.Animals

---
+++ Polymorphism - Lab/T02.Animals/Animal.cs
@@ -0,0 +1,44 @@
+﻿namespace Animals
+{
+    public class Animal
+    {
+        private string name;
+        private string favouriteFood;
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+
+        public string FavouriteFood
+        {
+            get
+            {
+                return this.favouriteFood;
+            }
+            set
+            {
+                this.favouriteFood = value;
+            }
+        }
+
+        public Animal(string name, string favouriteFood)
+        {
+            this.Name = name;
+            this.FavouriteFood = favouriteFood;
+        }
+
+        public virtual string ExplainSelf()
+        {
+            return string.Empty;
+        }
+    }
+}
+
