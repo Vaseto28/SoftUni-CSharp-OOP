daab6241fa2f330f21b5ce290c8634f4c6a7e3b5
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-14 22:24:21 +0200

    Completed task: T02.VehiclesExtention

---
+++ Polymorphism - Exercise/T02.VehiclesExtention/Bus.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace T02.VehiclesExtention
+{
+    public class Bus : Vehicle
+    {
+        public Bus(double fuelQuantity, double fuelConsumption, double tankCapacity) : base(fuelQuantity, fuelConsumption, tankCapacity)
+        {
+        }
+
+        public bool DriveNotEmpty(double distance)
+        {
+            this.FuelConsumption += 1.4;
+            double neededLitres = this.FuelConsumption * distance;
+            this.FuelConsumption -= 1.4;
+
+            if (this.FuelQuantity >= neededLitres)
+            {
+                this.FuelQuantity -= neededLitres;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override bool Refuel(double litres)
+        {
+            this.FuelQuantity += litres;
+
+            if (this.FuelQuantity > this.TankCapacity)
+            {
+                this.FuelQuantity -= litres;
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
+
---
+++ Polymorphism - Exercise/T02.VehiclesExtention/Vehicle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace T02.VehiclesExtention
+{
+    public abstract class Vehicle
+    {
+        public double FuelQuantity { get; set; }
+
+        public double FuelConsumption { get; set; }
+
+        public double TankCapacity { get; set; }
+
+        public Vehicle(double fuelQuantity, double fuelConsumption, double tankCapacity)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+            this.TankCapacity = tankCapacity;
+        }
+
+        public bool Drive(double distance)
+        {
+            double neededLitres = this.FuelConsumption * distance;
+
+            if (this.FuelQuantity >= neededLitres)
+            {
+                this.FuelQuantity -= neededLitres;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public abstract bool Refuel(double litres);
+    }
+}
+
