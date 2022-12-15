f540d83053dc8403524f4a66f7467998cf35dd2f
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-15 09:48:26 +0200

    Completed task: T01.CommandPattern

---
+++ Reflection and attributes - Exercise/T01.CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,36 @@
+﻿namespace CommandPattern.Core
+{
+    using System.Reflection;
+    using System.Linq;
+    using Contracts;
+    using System;
+
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] info = args.Split();
+            string cmdName = info[0];
+            string[] cmdArgs = info.Skip(1).ToArray();
+
+            Assembly assembly = Assembly.GetCallingAssembly();
+            Type cmdType = assembly?
+                .GetTypes()
+                .FirstOrDefault(x => x.Name == $"{cmdName}Command"
+                    && x.GetInterfaces().Any(x => x == typeof(ICommand)));
+
+            if (cmdType == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            object cmdInstance = Activator.CreateInstance(cmdType);
+            MethodInfo methodInfo = cmdType.GetMethods().First(m => m.Name == "Execute");
+
+            string result = (string)methodInfo.Invoke(cmdInstance, new object[] { cmdArgs });
+
+            return result;
+        }
+    }
+}
+
