f4e83c9b337c03a08e818c52916b0f5011ed65d2
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-15 09:52:03 +0200

    Completed task: T04.FightingArena

---
+++ Unit testing - Exercise/T04.FightingArena/FightingArena.Tests/ArenaTests.cs
@@ -0,0 +1,132 @@
+﻿
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using NUnit.Framework;
+    using FightingArena;
+
+    [TestFixture]
+    public class ArenaTests
+    {
+        //Test for the constructor
+        [Test]
+        public void TheConstructorShouldInitializeTheArenaCorrectly()
+        {
+            Arena arena = new Arena();
+
+            CollectionAssert.AreEqual(new List<Warrior>(), arena.Warriors, "TheConstructorShouldInitializeTheArenaCorrectly");
+        }
+
+        //Test for the .Count property
+        //We assume that the Enroll() method is working correctly!
+        [Test]
+        public void TheCountPropertyShouldReturnCorrectValue()
+        {
+            Arena arena = new Arena();
+
+            Warrior warrior = new Warrior("Pesho", 20, 60);
+            Warrior warrior1 = new Warrior("Gosho", 30, 400);
+
+            arena.Enroll(warrior);
+            arena.Enroll(warrior1);
+
+            int expectedCount = 2;
+            int actualCount = arena.Count;
+
+            Assert.AreEqual(expectedCount, actualCount, "TheCountPropertyShouldReturnCorrectValue");
+        }
+
+        //Tests for Enroll() method
+        [Test]
+        public void EnrollMethodShouldAddAWarriorInTheCollection()
+        {
+            Arena arena = new Arena();
+            Warrior warrior = new Warrior("Pesho", 20, 40);
+            Warrior warrior1 = new Warrior("Gosho", 20, 80);
+
+            arena.Enroll(warrior);
+            arena.Enroll(warrior1);
+
+            int expectedCount = 2;
+            int actualCount = arena.Count;
+
+            List<Warrior> expectedWarriors = new List<Warrior>
+            {
+                new Warrior("Pesho", 20, 40),
+                new Warrior("Gosho", 20, 80)
+            };
+
+            List<Warrior> actualWarriors = arena.Warriors.ToList();
+
+            CollectionAssert.AreEqual(expectedWarriors, actualWarriors, "EnrollMethodShouldAddAWarriorInTheCollection");
+            Assert.AreEqual(expectedCount, actualCount, "EnrollMethodShouldAddAWarriorInTheCollection");
+        }
+
+        [Test]
+        public void EnrollMethodMustNotAddTheWarriorIfTheCollectionContainsWarriorWithTheSameName()
+        {
+            Arena arena = new Arena();
+
+            Warrior warrior = new Warrior("Pesho", 20, 60);
+
+            arena.Enroll(warrior);
+
+            Warrior warriorToNotAdd = new Warrior("Pesho", 80, 90);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                arena.Enroll(warriorToNotAdd);
+            }, "Warrior is already enrolled for the fights!");
+        }
+
+        //Tests for the Fight() method
+        [Test]
+        public void InFightMethodAttackerShouldAttackTheDefender()
+        {
+            Arena arena = new Arena();
+            Warrior attacker = new Warrior("Pesho", 20, 100);
+            Warrior defender = new Warrior("Angel", 30, 90);
+
+            arena.Enroll(attacker);
+            arena.Enroll(defender);
+
+            arena.Fight("Pesho", "Angel");
+
+            int expectedAttackerHP = 70;
+            int actualAttackerHP = attacker.HP;
+
+            int expectedDefenderHp = 70;
+            int actualDefenderHP = defender.HP;
+
+            Assert.AreEqual(expectedAttackerHP, actualAttackerHP, "InFightMethodAttackerShouldAttackTheDefender");
+            Assert.AreEqual(expectedDefenderHp, actualDefenderHP, "InFightMethodAttackerShouldAttackTheDefender");
+        }
+
+        [Test]
+        public void InFightMethodAttackerCannotBeNull()
+        {
+            Arena arena = new Arena();
+            Warrior defender = new Warrior("Angel", 30, 90);
+
+            arena.Enroll(defender);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                arena.Fight("Pesho", "Angel");
+            }, $"There is no fighter with name Pesho enrolled for the fights!");
+        }
+
+        [Test]
+        public void InFightMethodDefenderCannotBeNull()
+        {
+            Arena arena = new Arena();
+            Warrior attacker = new Warrior("Pesho", 30, 90);
+
+            arena.Enroll(attacker);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                arena.Fight("Pesho", "Angel");
+            }, $"There is no fighter with name Angel enrolled for the fights!");
+        }
+    }
---
+++ Unit testing - Exercise/T04.FightingArena/FightingArena.Tests/WarriorTests.cs
@@ -0,0 +1,167 @@
+namespace FightingArena.Tests
+{
+    using System;
+    using NUnit.Framework;
+    using FightingArena;
+
+    [TestFixture]
+    public class WarriorTests
+    {
+        private const int MIN_ATTACK_HP = 30;
+
+        //Test for the constructor
+        [Test]
+        public void ConstructorShouldInitializeTheWarrior()
+        {
+            Warrior warrior = new Warrior("Angel", 20, 400);
+
+            Assert.That(warrior.Name == "Angel", "ConstructorShouldInitializeTheWarrior");
+            Assert.That(warrior.Damage == 20, "ConstructorShouldInitializeTheWarrior");
+            Assert.That(warrior.HP == 400, "ConstructorShouldInitializeTheWarrior");
+        }
+
+        //Test for the .Name property
+        [Test]
+        public void NamePropertyCannotBeNullOrWhiteSpace()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                Warrior warrior = new Warrior(null, 2, 2);
+                Warrior warrior1 = new Warrior(" ", 2, 5);
+            }, "Name should not be empty or whitespace!");
+        }
+
+        [Test]
+        public void NameGetterShouldReturnTheCorrectValue()
+        {
+            Warrior warrior = new Warrior("NNN", 20, 20);
+
+            string expectedName = "NNN";
+            string actualName = warrior.Name;
+
+            Assert.AreEqual(expectedName, actualName, "NameGetterShouldReturnTheCorrectValue");
+        }
+
+        //Tests for the .Damage property
+        [Test]
+        public void DamagePropertyCannotBeZeroOrNegative()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                Warrior warrior = new Warrior("Name", 0, 20);
+                Warrior warrior1 = new Warrior("name1", -20, 20);
+            }, "Damage value should be positive!");
+        }
+
+        [Test]
+        public void DamageGetterShouldReturnCorrectValue()
+        {
+            Warrior warrior = new Warrior("NNN", 20, 20);
+
+            int expectedDamage = 20;
+            int actualDamage = warrior.Damage;
+
+            Assert.AreEqual(expectedDamage, actualDamage, "NameGetterShouldReturnTheCorrectValue");
+        }
+
+        //Tests for the .HP property
+        [Test]
+        public void HPPropertyCannotBeNegative()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                Warrior warrior = new Warrior("name", 20, -20);
+            }, "HP should not be negative!");
+        }
+
+        [Test]
+        public void HPGetterShouldReturnTheCorrectValue()
+        {
+            Warrior warrior = new Warrior("NNN", 20, 20);
+
+            int expectedHP = 20;
+            int actualHp = warrior.HP;
+
+            Assert.AreEqual(expectedHP, actualHp, "NameGetterShouldReturnTheCorrectValue");
+        }
+
+        //Tests for the Attack() method
+        [Test]
+        public void YourHPMustBeMoreThanMIN_ATTACK_HP()
+        {
+            Warrior attacker = new Warrior("Attacker", 200, 26);
+            Warrior defender = new Warrior("Defender", 20, 40);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                attacker.Attack(defender);
+            }, "Your HP is too low in order to attack other warriors!");
+        }
+
+        [Test]
+        public void TheHPOfYourOppenentMustBeMoreThanMIN_ATTACK_HP()
+        {
+            Warrior attacker = new Warrior("Attacker", 200, 96);
+            Warrior defender = new Warrior("Defender", 20, 10);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                attacker.Attack(defender);
+            }, $"Enemy HP must be greater than {MIN_ATTACK_HP} in order to attack him!");
+        }
+
+        [Test]
+        public void YourHPMustBeMoreThanTheDamageOfYourOpponent()
+        {
+            Warrior attacker = new Warrior("Attacker", 20, 96);
+            Warrior defender = new Warrior("Defender", 200, 10);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                attacker.Attack(defender);
+            }, $"You are trying to attack too strong enemy");
+        }
+
+        [Test]
+        public void AttackMethodShouldReduceYourHPByTheDefenderDamage()
+        {
+            Warrior attacker = new Warrior("Attacker", 20, 96);
+            Warrior defender = new Warrior("Defender", 20, 70);
+
+            attacker.Attack(defender);
+
+            int expectedHP = 76;
+            int actualHP = attacker.HP;
+
+            Assert.AreEqual(expectedHP, actualHP, "AttackMethodShouldReduceYourHPByTheDefenderDamage");
+        }
+
+        [Test]
+        public void AttackMethodShouldKillTheDefenderIfHisHPIsLessThanTheDamageOfTheAttacker()
+        {
+            Warrior attacker = new Warrior("Attacker", 200, 96);
+            Warrior defender = new Warrior("Defender", 20, 70);
+
+            attacker.Attack(defender);
+
+            int expectedDefenderHP = 0;
+            int actualDefenderHp = defender.HP;
+
+            Assert.AreEqual(expectedDefenderHP, actualDefenderHp, "AttackMethodShouldKillTheDefenderIfHisHPIsLessThanTheDamageOfTheAttacker");
+        }
+
+        [Test]
+        public void AttackMethodShouldReduceTheHPOfTheDefenderIfTheDamagaOfAttackerIsLessThanTheHPOfTheDefender()
+        {
+            Warrior attacker = new Warrior("Attacker", 43, 96);
+            Warrior defender = new Warrior("Defender", 20, 70);
+
+            attacker.Attack(defender);
+
+            int expectedDefenderHP = 27;
+            int actualDefenderHp = defender.HP;
+
+            Assert.AreEqual(expectedDefenderHP, actualDefenderHp, "AttackMethodShouldReduceTheHPOfTheDefenderIfTheDamagaOfAttackerIsLessThanTheHPOfTheDefender");
+        }
+    }
+}
