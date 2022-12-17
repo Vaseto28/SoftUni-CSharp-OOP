d955349f222bd2d702b2a0cc71bb403e1b9a6bbf
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-18 00:06:16 +0200

    Completed task: Created UnitRepository class

---
+++ Exam preparations/01. First exam preparation/First exam preparation - business logic/FirstExamPreparation/Business logic/Repositories/UnitRepository.cs
@@ -0,0 +1,26 @@
+﻿namespace PlanetWars.Repositories
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Contracts;
+    using PlanetWars.Models.MilitaryUnits.Contracts;
+
+    public class UnitRepository : IRepository<IMilitaryUnit>
+    {
+        private readonly HashSet<IMilitaryUnit> militaryUnits;
+
+        public UnitRepository()
+        {
+            this.militaryUnits = new HashSet<IMilitaryUnit>();
+        }
+
+        public IReadOnlyCollection<IMilitaryUnit> Models => this.militaryUnits;
+
+        public void AddItem(IMilitaryUnit model) => this.militaryUnits.Add(model);
+
+        public IMilitaryUnit FindByName(string name) => this.militaryUnits.FirstOrDefault(x => x.GetType().Name == name);
+
+        public bool RemoveItem(string name) => this.militaryUnits.Remove(this.militaryUnits.FirstOrDefault(x => x.GetType().Name == name));
+    }
+}
+
