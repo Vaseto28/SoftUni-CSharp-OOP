366f64b1092960c574e4d33f735700f9899782d1
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-18 00:06:00 +0200

    Completed task: Created PlanetRepository class

---
+++ Exam preparations/01. First exam preparation/First exam preparation - business logic/FirstExamPreparation/Business logic/Repositories/PlanetRepository.cs
@@ -0,0 +1,27 @@
+﻿namespace PlanetWars.Repositories
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using PlanetWars.Models.Planets.Contracts;
+    using PlanetWars.Repositories.Contracts;
+
+    public class PlanetRepository : IRepository<IPlanet>
+    {
+        private readonly HashSet<IPlanet> planets;
+
+        public PlanetRepository()
+        {
+            this.planets = new HashSet<IPlanet>();
+        }
+
+        public IReadOnlyCollection<IPlanet> Models => this.planets;
+
+        public void AddItem(IPlanet model) => this.planets.Add(model);
+
+        public IPlanet FindByName(string name) => this.planets.FirstOrDefault(x => x.Name == name);
+
+        public bool RemoveItem(string name) => this.planets.Remove(this.planets.FirstOrDefault(x => x.Name == name));
+    }
+}
+
