a80c8855bb5e56840956774a34724069e3571778
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-18 00:03:51 +0200

    Completed task: Created Weapon class

---
+++ Exam preparations/01. First exam preparation/First exam preparation - business logic/FirstExamPreparation/Business logic/Models/Weapons/Weapon.cs
@@ -0,0 +1,39 @@
+﻿namespace PlanetWars.Models.Weapons
+{
+    using Contracts;
+    using PlanetWars.Utilities.Messages;
+    using System;
+
+    public abstract class Weapon : IWeapon
+    {
+        private int destructionLevel;
+
+        public Weapon(int destructionLevel, double price)
+        {
+            this.DestructionLevel = destructionLevel;
+            this.Price = price;
+        }
+
+        public double Price { get; private set; }
+
+        public int DestructionLevel
+        {
+            get => this.destructionLevel;
+            private set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentException(ExceptionMessages.TooLowDestructionLevel);
+                }
+
+                if (value > 10)
+                {
+                    throw new ArgumentException(ExceptionMessages.TooHighDestructionLevel);
+                }
+
+                this.destructionLevel = value;
+            }
+        }
+    }
+}
+
