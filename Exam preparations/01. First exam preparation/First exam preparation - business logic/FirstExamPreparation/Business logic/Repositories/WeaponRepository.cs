bcc6ab9275dbf03d3b6abac1e2a34944653b3a40
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-19 10:01:06 +0200

    Completed task: Created WeaponRepository class

---
+++ Exam preparations/01. First exam preparation/First exam preparation - business logic/FirstExamPreparation/Business logic/Repositories/WeaponRepository.cs
@@ -0,0 +1,26 @@
+﻿namespace PlanetWars.Repositories
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using PlanetWars.Models.Weapons.Contracts;
+    using PlanetWars.Repositories.Contracts;
+
+    public class WeaponRepository : IRepository<IWeapon>
+    {
+        private readonly HashSet<IWeapon> weapons;
+
+        public WeaponRepository()
+        {
+            this.weapons = new HashSet<IWeapon>();
+        }
+
+        public IReadOnlyCollection<IWeapon> Models => this.weapons;
+
+        public void AddItem(IWeapon model) => this.weapons.Add(model);
+
+        public IWeapon FindByName(string name) => this.weapons.FirstOrDefault(x => x.GetType().Name == name);
+
+        public bool RemoveItem(string name) => this.weapons.Remove(this.weapons.FirstOrDefault(x => x.GetType().Name == name));
+    }
+}
+
