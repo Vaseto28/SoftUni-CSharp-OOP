6356ee29739c8023fe29c59ee7c8ba891a70f90f
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-20 11:11:09 +0200

    Completed task: Created HeroRepository class

---
+++ Exam preparations/02. Second exam preparation/Second exam preparation - business logic/Heroes/Repositories/HeroRepository.cs
@@ -0,0 +1,27 @@
+﻿namespace Heroes.Repositories
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Heroes.Models.Contracts;
+    using Heroes.Repositories.Contracts;
+
+    public class HeroRepository : IRepository<IHero>
+    {
+        private readonly HashSet<IHero> heroes;
+
+        public HeroRepository()
+        {
+            this.heroes = new HashSet<IHero>();
+        }
+
+        public IReadOnlyCollection<IHero> Models => this.heroes;
+
+        public void Add(IHero model) => this.heroes.Add(model);
+
+        public IHero FindByName(string name) => this.heroes.FirstOrDefault(x => x.Name == name);
+
+        public bool Remove(IHero model) => this.heroes.Remove(model);
+    }
+}
+
