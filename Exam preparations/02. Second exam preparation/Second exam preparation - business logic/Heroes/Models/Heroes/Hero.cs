fa8bd1920d64e65852bb962ea63cbab430bc0601
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-20 11:09:06 +0200

    Completed task: Created Hero class

---
+++ Exam preparations/02. Second exam preparation/Second exam preparation - business logic/Heroes/Models/Heroes/Hero.cs
@@ -0,0 +1,109 @@
+﻿namespace Heroes.Models.Heroes
+{
+    using Contracts;
+    using System;
+
+    public class Hero : IHero
+    {
+        private string name;
+        private int health;
+        private int armour;
+        private IWeapon weapon;
+
+        public Hero(string name, int health, int armour)
+        {
+            this.Name = name;
+            this.Health = health;
+            this.Armour = armour;
+        }
+
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Hero name cannot be null or empty.");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public int Health
+        {
+            get => this.health;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Hero health cannot be below 0.");
+                }
+
+                this.health = value;
+            }
+        }
+
+        public int Armour
+        {
+            get => this.armour;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Hero armour cannot be below 0.");
+                }
+
+                this.armour = value;
+            }
+        }
+
+        public IWeapon Weapon
+        {
+            get => this.weapon;
+            private set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentException("Weapon cannot be null.");
+                }
+
+                this.weapon = value;
+            }
+        }
+
+        public bool IsAlive => this.Health > 0;
+
+        public void AddWeapon(IWeapon weapon)
+        {
+            if (this.Weapon == null)
+            {
+                this.Weapon = weapon;
+            }
+        }
+
+        public void TakeDamage(int points)
+        {
+            if (this.Armour <= points)
+            {
+                int pointsLeft = points - this.Armour;
+                this.Armour = 0;
+
+                if (this.Health <= pointsLeft)
+                {
+                    this.Health = 0;
+                }
+                else
+                {
+                    this.Health -= pointsLeft;
+                }
+            }
+            else
+            {
+                this.Armour -= points;
+            }
+        }
+    }
+}
+
