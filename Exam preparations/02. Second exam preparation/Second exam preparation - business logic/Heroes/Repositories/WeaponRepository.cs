b008e336afc46529c7c92a70b1fcdc755c0230f7
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-20 11:11:23 +0200

    Completed task: Created WeaponRepository class

---
+++ Exam preparations/02. Second exam preparation/Second exam preparation - business logic/Heroes/Repositories/WeaponRepository.cs
@@ -0,0 +1,27 @@
+﻿namespace Heroes.Repositories
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Heroes.Models.Contracts;
+    using Heroes.Repositories.Contracts;
+
+    public class WeaponRepository : IRepository<IWeapon>
+    {
+        private readonly HashSet<IWeapon> weapons;
+
+        public WeaponRepository()
+        {
+            this.weapons = new HashSet<IWeapon>();
+        }
+
+        public IReadOnlyCollection<IWeapon> Models => this.weapons;
+
+        public void Add(IWeapon model) => this.weapons.Add(model);
+
+        public IWeapon FindByName(string name) => this.weapons.FirstOrDefault(x => x.Name == name);
+
+        public bool Remove(IWeapon model) => this.weapons.Remove(model);
+    }
+}
+
