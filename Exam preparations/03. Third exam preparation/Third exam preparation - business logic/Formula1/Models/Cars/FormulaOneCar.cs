92a9f1385123fa1c83851cadee1b0ded23413bb4
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-21 14:35:36 +0200

    Completed task: Created FormulaOneCar class
    This reverts commit 1c3c8911fc1114ce5a79b124f2634f359a844076.

---
+++ Exam preparations/03. Third exam preparation/Third exam preparation - business logic/Formula1/Models/Cars/FormulaOneCar.cs
@@ -0,0 +1,65 @@
+﻿namespace Formula1.Models
+{
+    using System;
+    using Contracts;
+    using Formula1.Utilities;
+
+    public abstract class FormulaOneCar : IFormulaOneCar
+    {
+        private string model;
+        private int horsepower;
+        private double engineDisplacement;
+
+        public FormulaOneCar(string model, int horsepower, double engineDisplacement)
+        {
+            this.Model = model;
+            this.Horsepower = horsepower;
+            this.EngineDisplacement = engineDisplacement;
+        }
+
+        public string Model
+        {
+            get => this.model;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < 3)
+                {
+                    throw new ArgumentException(String.Format(ExceptionMessages.InvalidF1CarModel, value));
+                }
+
+                this.model = value;
+            }
+        }
+
+        public int Horsepower
+        {
+            get => this.horsepower;
+            private set
+            {
+                if (value < 900 || value > 1050)
+                {
+                    throw new ArgumentException(String.Format(ExceptionMessages.InvalidF1HorsePower, value));
+                }
+
+                this.horsepower = value;
+            }
+        }
+
+        public double EngineDisplacement
+        {
+            get => this.engineDisplacement;
+            private set
+            {
+                if (value < 1.6 || value > 2.00)
+                {
+                    throw new ArgumentException(String.Format(ExceptionMessages.InvalidF1EngineDisplacement, value));
+                }
+
+                this.engineDisplacement = value;
+            }
+        }
+
+        public double RaceScoreCalculator(int laps) => this.EngineDisplacement / this.Horsepower * laps;
+    }
+}
+
