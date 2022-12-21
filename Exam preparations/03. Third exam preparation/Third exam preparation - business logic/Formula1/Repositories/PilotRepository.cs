33e559ffa7f90a36c4bbfb10728663ceb5466f00
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-21 14:37:29 +0200

    Completed task: Created PilotRepository class

---
+++ Exam preparations/03. Third exam preparation/Third exam preparation - business logic/Formula1/Repositories/PilotRepository.cs
@@ -0,0 +1,27 @@
+﻿namespace Formula1.Repositories
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Formula1.Models.Contracts;
+    using Formula1.Repositories.Contracts;
+
+    public class PilotRepository : IRepository<IPilot>
+    {
+        private readonly HashSet<IPilot> pilots;
+
+        public PilotRepository()
+        {
+            this.pilots = new HashSet<IPilot>();
+        }
+
+        public IReadOnlyCollection<IPilot> Models => this.pilots;
+
+        public void Add(IPilot model) => this.pilots.Add(model);
+
+        public IPilot FindByName(string name) => this.pilots.FirstOrDefault(x => x.FullName == name);
+
+        public bool Remove(IPilot model) => this.pilots.Remove(model);
+    }
+}
+
