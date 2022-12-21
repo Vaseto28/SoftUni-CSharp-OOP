29da6ccd74a4b6bfefe2d922013b32b24c8d85bb
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-21 14:38:24 +0200

    Completed task: Created RaceRepository class

---
+++ Exam preparations/03. Third exam preparation/Third exam preparation - business logic/Formula1/Repositories/RaceRepository.cs
@@ -0,0 +1,27 @@
+﻿namespace Formula1.Repositories
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Formula1.Models.Contracts;
+    using Formula1.Repositories.Contracts;
+
+    public class RaceRepository : IRepository<IRace>
+    {
+        private readonly HashSet<IRace> races;
+
+        public RaceRepository()
+        {
+            this.races = new HashSet<IRace>();
+        }
+
+        public IReadOnlyCollection<IRace> Models => this.races;
+
+        public void Add(IRace model) => this.races.Add(model);
+
+        public IRace FindByName(string name) => this.races.FirstOrDefault(x => x.RaceName == name);
+
+        public bool Remove(IRace model) => this.races.Remove(model);
+    }
+}
+
