c2ce5e3c30f01a9970f72f527ef91f775544e0df
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-21 14:36:58 +0200

    Completed task: Created FormulaOneCarRepository class

---
+++ Exam preparations/03. Third exam preparation/Third exam preparation - business logic/Formula1/Repositories/FormulaOneCarRepository.cs
@@ -0,0 +1,27 @@
+﻿namespace Formula1.Repositories
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Formula1.Models.Contracts;
+    using Formula1.Repositories.Contracts;
+
+    public class FormulaOneCarRepository : IRepository<IFormulaOneCar>
+    {
+        private readonly HashSet<IFormulaOneCar> cars;
+
+        public FormulaOneCarRepository()
+        {
+            this.cars = new HashSet<IFormulaOneCar>();
+        }
+
+        public IReadOnlyCollection<IFormulaOneCar> Models => this.cars;
+
+        public void Add(IFormulaOneCar model) => this.cars.Add(model);
+
+        public IFormulaOneCar FindByName(string name) => this.cars.FirstOrDefault(x => x.Model == name);
+
+        public bool Remove(IFormulaOneCar model) => this.cars.Remove(model);
+    }
+}
+
