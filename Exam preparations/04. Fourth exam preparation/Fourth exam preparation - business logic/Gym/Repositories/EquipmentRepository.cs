d63e08b3515c35faccaa0d37d961220449b5cd91
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-23 13:24:38 +0200

    Completed task: Created EquipmentRepository class

---
+++ Exam preparations/04. Fourth exam preparation/Fourth exam preparation - business logic/Gym/Repositories/EquipmentRepository.cs
@@ -0,0 +1,27 @@
+﻿namespace Gym.Repositories
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Gym.Models.Equipment.Contracts;
+    using Gym.Repositories.Contracts;
+
+    public class EquipmentRepository : IRepository<IEquipment>
+    {
+        private List<IEquipment> equipment;
+
+        public EquipmentRepository()
+        {
+            this.equipment = new List<IEquipment>();
+        }
+
+        public IReadOnlyCollection<IEquipment> Models => this.equipment;
+
+        public void Add(IEquipment model) => this.equipment.Add(model);
+
+        public IEquipment FindByType(string type) => this.equipment.FirstOrDefault(x => x.GetType().Name == type);
+
+        public bool Remove(IEquipment model) => this.equipment.Remove(model);
+    }
+}
+
