54c7a7a67a41abe3b8176aa962e8bbd4030a1668
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-24 16:48:59 +0200

    Completed task: Created RacerRepository class

---
+++ Exam preparations/05. Fifth exam preparation/Fifth exam preparation - business logic/CarRacing/Repositories/RacerRepository.cs
@@ -0,0 +1,36 @@
+﻿namespace CarRacing.Repositories
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using CarRacing.Models.Racers.Contracts;
+    using CarRacing.Repositories.Contracts;
+    using CarRacing.Utilities.Messages;
+
+    public class RacerRepository : IRepository<IRacer>
+    {
+        private readonly List<IRacer> racers;
+
+        public RacerRepository()
+        {
+            this.racers = new List<IRacer>();
+        }
+
+        public IReadOnlyCollection<IRacer> Models => this.racers;
+
+        public void Add(IRacer model)
+        {
+            if (model == null)
+            {
+                throw new ArgumentException(ExceptionMessages.InvalidAddRacerRepository);
+            }
+
+            this.racers.Add(model);
+        }
+
+        public IRacer FindBy(string property) => this.racers.FirstOrDefault(x => x.Username == property);
+
+        public bool Remove(IRacer model) => this.racers.Remove(model);
+    }
+}
+
