aa820d17344da2d637b8f84fbaedb4427e3bcbe9
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-24 16:48:41 +0200

    Completed task: Created CarRepository class

---
+++ Exam preparations/05. Fifth exam preparation/Fifth exam preparation - business logic/CarRacing/Repositories/CarRepository.cs
@@ -0,0 +1,36 @@
+﻿namespace CarRacing.Repositories
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using CarRacing.Models.Cars.Contracts;
+    using CarRacing.Repositories.Contracts;
+    using CarRacing.Utilities.Messages;
+
+    public class CarRepository : IRepository<ICar>
+    {
+        private readonly List<ICar> cars;
+
+        public CarRepository()
+        {
+            this.cars = new List<ICar>();
+        }
+        
+        public IReadOnlyCollection<ICar> Models => this.cars;
+
+        public void Add(ICar model)
+        {
+            if (model == null)
+            {
+                throw new ArgumentException(ExceptionMessages.InvalidAddCarRepository);
+            }
+
+            this.cars.Add(model);
+        }
+
+        public ICar FindBy(string property) => this.cars.FirstOrDefault(x => x.VIN == property);
+
+        public bool Remove(ICar model) => this.cars.Remove(model);
+    }
+}
+
