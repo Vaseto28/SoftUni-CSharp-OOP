1163a1451d7339e822eca12d6a79a8789fcd40ea
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-24 16:46:44 +0200

    Completed task: Created TunedCar class

---
+++ Exam preparations/05. Fifth exam preparation/Fifth exam preparation - business logic/CarRacing/Models/Cars/TunedCar.cs
@@ -0,0 +1,16 @@
+﻿namespace CarRacing.Models.Cars
+{
+    public class TunedCar : Car
+    {
+        public TunedCar(string make, string model, string VIN, int horsePower) : base(make, model, VIN, horsePower, 65, 7.5)
+        {
+        }
+
+        public override void Drive()
+        {
+            base.Drive();
+            this.FuelAvailable -= this.FuelAvailable * 0.03;
+        }
+    }
+}
+
