916d45c32e8d4d54e14a49dac9dbc91459935ecf
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-24 16:46:17 +0200

    Completed task: Created Car class

---
+++ Exam preparations/05. Fifth exam preparation/Fifth exam preparation - business logic/CarRacing/Models/Cars/Car.cs
@@ -0,0 +1,108 @@
+﻿namespace CarRacing.Models.Cars
+{
+    using System;
+    using CarRacing.Utilities.Messages;
+    using Contracts;
+
+    public abstract class Car : ICar
+    {
+        private string make;
+        private string model;
+        private string vin;
+        private int horsepower;
+        private double fuelConsumptionPerRace;
+
+        public Car(string make, string model, string VIN, int horsePower, double fuelAvailable, double fuelConsumptionPerRace)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.VIN = VIN;
+            this.HorsePower = horsePower;
+            this.FuelAvailable = fuelAvailable;
+            this.FuelConsumptionPerRace = fuelConsumptionPerRace;
+        }
+
+        public string Make
+        {
+            get => this.make;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidCarMake);
+                }
+
+                this.make = value;
+            }
+        }
+
+        public string Model
+        {
+            get => this.model;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidCarModel);
+                }
+
+                this.model = value;
+            }
+        }
+
+        public string VIN
+        {
+            get => this.vin;
+            private set
+            {
+                if (value.Length != 17)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidCarVIN);
+                }
+
+                this.vin = value;
+            }
+        }
+
+        public int HorsePower
+        {
+            get => this.horsepower;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidCarHorsePower);
+                }
+
+                this.horsepower = value;
+            }
+        }
+
+        public double FuelAvailable { get; protected set; }
+
+        public double FuelConsumptionPerRace
+        {
+            get => this.fuelConsumptionPerRace;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidCarFuelConsumption);
+                }
+
+                this.fuelConsumptionPerRace = value;
+            }
+        }
+
+        public virtual void Drive()
+        {
+            this.FuelAvailable -= this.fuelConsumptionPerRace;
+
+            if (this.FuelAvailable < 0)
+            {
+                this.FuelAvailable = 0;
+            }
+        }
+    }
+}
+
