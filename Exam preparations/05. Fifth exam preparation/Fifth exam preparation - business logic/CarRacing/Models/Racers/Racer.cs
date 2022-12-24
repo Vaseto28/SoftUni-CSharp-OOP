6bda95b89ab2a1bbce8df41084619d667676c14b
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-24 16:47:29 +0200

    Completed task: Created Racer class

---
+++ Exam preparations/05. Fifth exam preparation/Fifth exam preparation - business logic/CarRacing/Models/Racers/Racer.cs
@@ -0,0 +1,92 @@
+﻿namespace CarRacing.Models.Racers
+{
+    using System;
+    using CarRacing.Models.Cars.Contracts;
+    using CarRacing.Utilities.Messages;
+    using Contracts;
+
+    public abstract class Racer : IRacer
+    {
+        private string username;
+        private string racerBehavior;
+        private int drivingExperience;
+        private ICar car;
+
+        public Racer(string username, string racingBehavior, int drivingExperience, ICar car)
+        {
+            this.Username = username;
+            this.RacingBehavior = racingBehavior;
+            this.DrivingExperience = drivingExperience;
+            this.Car = car;
+        }
+
+        public string Username
+        {
+            get => this.username;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidRacerName);
+                }
+
+                this.username = value;
+            }
+        }
+
+        public string RacingBehavior
+        {
+            get => this.racerBehavior;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidRacerBehavior);
+                }
+
+                this.racerBehavior = value;
+            }
+        }
+
+        public int DrivingExperience
+        {
+            get => this.drivingExperience;
+            protected set
+            {
+                if (value < 0 || value > 100)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidRacerDrivingExperience);
+                }
+
+                this.drivingExperience = value;
+            }
+        }
+
+        public ICar Car
+        {
+            get => this.car;
+            private set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidRacerCar);
+                }
+
+                this.car = value;
+            }
+        }
+
+        public bool IsAvailable()
+        {
+            if (this.Car.FuelAvailable > 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public virtual void Race() => this.Car.Drive();
+    }
+}
+
