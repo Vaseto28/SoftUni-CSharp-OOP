09fca8b96de97b29af091ad6ac2bc03063cc368d
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-24 16:47:50 +0200

    Completed task: Created ProfessionalRacer class

---
+++ Exam preparations/05. Fifth exam preparation/Fifth exam preparation - business logic/CarRacing/Models/Racers/ProfessionalRacer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using CarRacing.Models.Cars.Contracts;
+
+namespace CarRacing.Models.Racers
+{
+    public class ProfessionalRacer : Racer
+    {
+        public ProfessionalRacer(string username, ICar car) : base(username, "strict", 30, car)
+        {
+        }
+
+        public override void Race()
+        {
+            base.Race();
+            this.DrivingExperience += 10;
+        }
+    }
+}
+
